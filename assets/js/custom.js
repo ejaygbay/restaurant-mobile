f17a32e5f1fb395906001b632f6e7c174761993c
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-08-30 12:18:03 +0000

    Menu items displayed dynamically.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,46 @@
+let menu_obj = {
+    bucket_of_seafood_chowder: {
+        item: "Bucket Of Seafood Chowder",
+        price: 13.24,
+        type: "food"
+    },
+    "sirloin_tips*_&_chicken_tenders_family_meal_deal": {
+        item: "Sirloin Tips* & Chicken Tenders Family Meal Deal",
+        price: 54.75,
+        type: "food"
+    },
+    french_fries: {
+        item: "French Fries",
+        price: 5.00,
+        type: "food"
+    },
+    gingerbread: {
+        item: "Gingerbread",
+        price: 4.00,
+        type: "food"
+    },
+    fresh_strawberry_margarita: {
+        item: "Fresh Strawberry Margarita",
+        price: 14.00,
+        type: "drink"
+    },
+    frozen_lemonades: {
+        item: "Frozen Lemonades",
+        price: 4.59,
+        type: "drink"
+    },
+    fresh_fruit_sangria: {
+        item: "Fresh Fruit Sangria",
+        price: 7.39,
+        type: "drink"
+    },
+    sparkling_refreshers: {
+        item: "Sparkling Refreshers",
+        price: 3.26,
+        type: "drink"
+    }
+}
+
 // General scripts
 let triggerTabList = [].slice.call(document.querySelectorAll('#nav-tab button'));
 console.log(triggerTabList);
@@ -32,6 +75,7 @@ document.querySelector("#after-menu-selection-btn").addEventListener("click", ()
 const hideSection = (ele) => {
     document.querySelector(ele).style = "display: none";
 }
+
 const showSection = (ele) => {
     document.querySelector(ele).style = "display: block";
 }
@@ -40,6 +84,29 @@ const showSection = (ele) => {
 /**
  * Food menu scripts
  */
+const displayMenuItems = (items_array) => {
+    let food_items = document.querySelector(`#food-menu-list`);
+    let drink_items = document.querySelector(`#drinks-menu-list`);
+    food_items.innerHTML = "";
+    drink_items.innerHTML = "";
+
+    items_array.forEach(ele => {
+        let html = `<div class="food-menu-item">
+            <label for="menu-${ele}">
+                <div class="menu-item-title">${menu_obj[ele].item}</div>
+                <div class="menu-item-price">$${menu_obj[ele].price}</div>
+            </label>
+            <div class="menu-item-selection">
+                <input type="checkbox" class="form-check-input" id="menu-${ele}">
+            </div>
+        </div>`;
+
+        menu_obj[ele].type === 'food' ? food_items.insertAdjacentHTML('beforeend', html) : drink_items.insertAdjacentHTML('beforeend', html);
+    })
+}
+
+displayMenuItems(Object.keys(menu_obj));
+
 document.querySelectorAll(".food-menu-item input[type=checkbox]").forEach(ele => {
     ele.addEventListener("change", (e) => {
         console.log(e.target.checked);
